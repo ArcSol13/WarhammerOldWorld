8289180dd113f5da704361ea4182a8892188e70f
robertbilic <robertb97b@gmail.com>
2020-10-25 21:11:55 +0100

    Custom module interface beginning

---
+++ WarhammerOldWorld/WarhammerOldWorld/CustomSubModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarhammerOldWorld
+{
+        public abstract class CustomSubModule
+        {
+            public CustomSubModule()
+            {
+                ModuleControllerSubModule.Instance.LoadCustomModule(this);
+            }
+            /// <summary>
+            /// Called every frame, avoid using costly operations
+            /// </summary>
+
+            public abstract void OnUpdate();
+
+            /// <summary>
+            /// Used to initialize module
+            /// </summary>
+            public abstract void OnLoad();
+
+            /// <summary>
+            /// Override to clean up after the module stops being used
+            /// </summary>
+            public abstract void OnUnload();
+        }
+
+}
--- WarhammerOldWorld/WarhammerOldWorld/Main.cs
+++ WarhammerOldWorld/WarhammerOldWorld/Main.cs
@@ -1,19 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using TaleWorlds.Core;
-using TaleWorlds.Localization;
 using TaleWorlds.MountAndBlade;
+using System.Reactive.Subjects;
+using TaleWorlds.Engine;
+using System.Reactive.Linq;
+using System.Collections.Generic;
 
 namespace WarhammerOldWorld
 {
-    public class Main : MBSubModuleBase
+    public class ModuleControllerSubModule : MBSubModuleBase
     {
+        public static ModuleControllerSubModule Instance { get; private set; }
+        List<CustomSubModule> activeModules = new List<CustomSubModule>();
+        public void LoadCustomModule(CustomSubModule module)
+        {
+            module.OnLoad();
+            activeModules.Add(module);
+        }
+        public void UnloadCustomModule(CustomSubModule module)
+        {
+            module.OnUnload();
+            activeModules.Remove(module);
+        }
 
 
+        BehaviorSubject<Scene> sceneSubject = new BehaviorSubject<Scene>(null);
+
+        protected override void OnApplicationTick(float dt)
+        {
+            base.OnApplicationTick(dt);
+            foreach (var module in activeModules)
+                module.OnUpdate();
+        }
+
+        protected override void OnSubModuleLoad()
+        {
+            Instance = this;
+
+        }
+
+        /// <summary>
+        /// Tracks scene changes, null at start
+        /// </summary>
+        /// <returns></returns>
+        public IObservable<Scene> SceneChangeObservable() => sceneSubject.AsObservable();
+        public Scene GetCurrentScene() => sceneSubject.Value;
 
     }
 }
