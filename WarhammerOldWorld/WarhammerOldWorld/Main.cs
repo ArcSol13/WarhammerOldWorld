3dc1714682b02314c2e301f5bbb3a9d8beb9cbad
LexOdin <dan.harris.solemnthoughts@gmail.com>
2020-10-25 15:04:14 -0500

    Initial Commit

---
+++ WarhammerOldWorld/WarhammerOldWorld/Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace WarhammerOldWorld
+{
+    public class Main : MBSubModuleBase
+    {
+
+
+
+    }
+}
