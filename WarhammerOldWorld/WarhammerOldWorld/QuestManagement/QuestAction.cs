152e8e93900c53312416650a28bf9e910824a091
William Cardoso <wcardosodev@hotmail.com>
2020-12-04 17:07:17 +0000

    Class testing

--- WarhammerOldWorld/WarhammerOldWorld/QuestManagement/QuestAction.cs
+++ WarhammerOldWorld/WarhammerOldWorld/QuestManagement/QuestAction.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
 
 namespace WarhammerOldWorld.QuestManagement
 {
-    class QuestAction
+    abstract class Quest
     {
+        protected abstract void OnComplete();
+
+        protected abstract void OnFail();
+
+        public string StartDialogue { get; set; }
+    }
+
+    class KillQuest : Quest
+    {
+        protected override void OnComplete()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnFail()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
