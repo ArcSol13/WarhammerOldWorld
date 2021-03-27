7ed78899b89d99ab132ffb9679e460750ebdf49b
Ruihan <kz14365@gmail.com>
2021-03-26 17:46:48 -0700

    Add files via upload

---
+++ OldWorld/OldWorld/SubModule.cs
@@ -0,0 +1,27 @@
+﻿using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.CampaignSystem;
+using System.Collections.Generic;
+using System;
+using HarmonyLib;
+
+namespace OldWorld
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+        }
+
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+        }
+
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            new Harmony("OldWorld").PatchAll();
+        }
+    }
+}
