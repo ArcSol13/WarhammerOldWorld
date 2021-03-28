5f48b7aaac9752a62f97ae8505000ca445129138
tristanka <tristanarm@gmail.com>
2021-03-28 18:49:51 -0500

    Update HealingAura with adjustments
    This commit includes a few adjustments to HealingAuraAgentComponent:
    
    1. Utilize new agent.Heal extension method
    2. Spread out CPU load through time by randomizing start time of healing
       cycle per agent

--- WarhammerOldWorld/WarhammerOldWorld/CustomAgentComponents/HealingAuraAgentComponent.cs
+++ WarhammerOldWorld/WarhammerOldWorld/CustomAgentComponents/HealingAuraAgentComponent.cs
@@ -2,43 +2,56 @@
 using System.Collections.Generic;
 using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
+using WarhammerOldWorld.Combat;
 
 namespace WarhammerOldWorld.CustomAgentComponents
 {
     public class HealingAuraAgentComponent : AgentComponent
     {
         private Timer healTimer;
-        private float secondsBetweenHealProcs = 1;
-        private bool canHeal = false;
+        private float secondsBetweenHealProcs = 5;
+        private bool healReady = false;
         private float healRadius = 5;
-        private float healAmount = 1;
+        private float healAmount = 5;
 
         private Mission currentMission;
         public HealingAuraAgentComponent(Agent agent, Mission mission) : base(agent)
         {
             currentMission = mission;
-            float time = MBCommon.GetTime(MBCommon.TimeType.Mission);
-            this.healTimer = new Timer(time, secondsBetweenHealProcs, true);
+            this.healTimer = new Timer(GetHealStartTime(), secondsBetweenHealProcs, true);
         }
 
         protected override void OnTickAsAI(float dt)
         {
-            canHeal = healTimer.Check(MBCommon.GetTime(MBCommon.TimeType.Mission));
-            if (canHeal)
+            healReady = healTimer.Check(MBCommon.GetTime(MBCommon.TimeType.Mission));
+            if (healReady)
             {
                 IEnumerable<Agent> nearbyAgents = currentMission.GetNearbyAgents(Agent.GetWorldPosition().AsVec2, healRadius);
                 foreach (Agent agent in nearbyAgents)
                 {
-                    if(!agent.Team.IsEnemyOf(Agent.Team))
+                    float maxHitPoints = agent.Character.MaxHitPoints();
+                    if (!agent.Team.IsEnemyOf(Agent.Team) && agent.Health != maxHitPoints)
                     {
-                        float maxHitPoints = agent.Character.MaxHitPoints();
-                        agent.Health = Math.Min(agent.Health + healAmount, maxHitPoints);
-                        Helpers.Say(Agent.Name + " healed " + agent.Character.Name + " from " + (agent.Health - 1) + " to " + agent.Health);
+                        string origHealth = agent.Health.ToString();
+
+                        //Prevent overhealing with Math.Min
+                        agent.SyncHealthToClient();
+                        agent.Heal(healAmount);
+                        Helpers.Say(Agent.Name + " healed " + agent.Character.Name + " from " + origHealth + " to " + agent.Health);
                     }
                 }
                 healTimer.Reset(MBCommon.GetTime(MBCommon.TimeType.Mission));
             }
-            
+        }
+
+        /// <summary>
+        /// Get a random float between 0 and the healing frequency so agents that have healing auras won't all heal simultaneously.
+        /// Otherwise, there's a periodic high CPU load.
+        /// </summary>
+        /// <returns>float between 0 and the healing frequency</returns>
+        private float GetHealStartTime()
+        {
+            return MBCommon.GetTime(MBCommon.TimeType.Mission) - (float)Helpers.rng.NextDouble() * secondsBetweenHealProcs;
         }
     }
 }
